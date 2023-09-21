61c3ccfaf08a0bc65885b285a68a5a7458ac5ccf
Ali Nurdin <ali.nrdn14005@gmail.com>
2023-09-22 00:51:45 +0700

    add indeksis & FK tbl order

--- database/migrations/2023_09_21_091713_create_orders_table.php
+++ database/migrations/2023_09_21_091713_create_orders_table.php
@@ -13,13 +13,20 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('code_id');
-            $table->integer('user_id');
-            $table->integer('product_id');
+            $table->string('code_id')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('product_id'); 
+            $table->foreign('product_id')->references('id')->on('products');  
             $table->integer('price');
             $table->integer('qty');
             $table->integer('total');
             $table->timestamps();
+        });
+
+        // Add an index on code_id
+        Schema::table('orders', function (Blueprint $table) {
+            $table->index('code_id');
         });
     }
 
@@ -28,6 +35,12 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropIndex(['code_id']);
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['product_id']);  
+        });
+
         Schema::dropIfExists('orders');
     }
 };
