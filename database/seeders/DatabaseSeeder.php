633336c9693d0ef25f6e7df9c0d0cfcda541b345
Ali Nurdin <ali.nrdn14005@gmail.com>
2023-09-21 23:09:13 +0700

    db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Ali Nurdin',
+            'email' => 'ali.nrdn14005@gmail.com',
+            'password' => Hash::make('ali.nrdn14005@gmail.com'),
+            'role' => 1,  
+        ]
+    );
     }
 }
+    
