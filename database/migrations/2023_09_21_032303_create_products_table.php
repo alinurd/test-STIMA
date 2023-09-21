25c39c100e0288d13fcbda9d57df979046fd9d4b
Ali Nurdin <ali.nrdn14005@gmail.com>
2023-09-21 11:18:30 +0700

    create tbl product

--- database/migrations/2023_09_21_032303_create_products_table.php
+++ database/migrations/2023_09_21_032303_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->integer('price');
+            $table->integer('stok');
+            $table->integer('category')->default(0);
+            $table->string('create_at');
+            $table->string('update_at');
             $table->timestamps();
         });
     }
